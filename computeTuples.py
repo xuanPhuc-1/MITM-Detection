797def0e7ef76db18a0843a58e7d1344716fa847
xuanPhuc-1 <20021568@vnu.edu.vn>
2023-05-30 18:27:36 +0700

    18:27

--- computeTuples.py
+++ computeTuples.py
@@ -29,31 +29,7 @@ f.close()
 sfe = n_ip // 3
 
 # Number of interactive flow entries
-# Ratio of Pair-Flow Entries (RFIP)
-# RFIP = Interactive flow entries / total number of flows in T period
-fileone = None
-filetwo = None
 
-with open('data/ipsrc.csv', 'r') as t1, open('data/ipdst.csv', 'r') as t2:
-    fileone = t1.readlines()
-    filetwo = t2.readlines()
-
-# Check if the src_ip exists in the dst_ip,
-# which indicates that source IP has two-way interaction with the destination IP. 
-# If not, append that one-way interaction IP into interactive flow file (intflow.csv)
-with open('data/intflow.csv','w') as f:
-    for line in fileone:
-        if line not in filetwo:
-            f.write(line)
-
-# Count number of 
-with open('data/intflow.csv') as f:
-    reader = csv.reader(f, delimiter=",")
-    dt = list(reader)
-    row_count_nonint = len(dt)
-
-
-rfip = abs(float(n_ip - row_count_nonint) / n_ip)
 headers = ["SSIP", "SDFP", "SDFB", "SFE", "RFIP", "ENTROPY"]
 
 features = [ssip, sdfp, sdfb, sfe, rfip, entropy.value]
