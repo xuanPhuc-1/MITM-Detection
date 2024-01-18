3aad0343fa7cd1ef256dd724748e8e2e111d6b54
xuanPhuc-1 <boneman1782002@gmail.com>
2024-01-18 17:30:34 +0700

    add dataset

--- Support tools/add_row.py
+++ Support tools/add_row.py
@@ -14,11 +14,11 @@ with open(output_file_path, mode='w', newline='') as file:
     # Ghi dữ liệu với giá trị ngẫu nhiên
     for _ in range(num_rows):
         aps = round(random.uniform(20.0, 30.0), 3)
-        abps = round(random.uniform(3.0, 4.0), 3)
+        abps = 0
         subarp = int(random.uniform(1, 4))
         # miss_mac random 0 or 1
-        miss_mac = 1
-        class_value = 1
+        miss_mac = 0
+        class_value = 0
         writer.writerow([aps, abps, subarp, miss_mac, class_value])
 
 print(f"File '{output_file_path}' đã được tạo thành công.")
